726e55afe08fc4ecc3afa2bd6c3dff164c7eaabc
Haruaki <32154385+haruaki07@users.noreply.github.com>
2024-04-19 01:45:38 +0000

    fix: rename smtp env vars

--- src/config.js
+++ src/config.js
@@ -11,10 +11,10 @@ const config = {
     url: process.env.RABBITMQ_SERVER
   },
   smtp: {
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    user: process.env.MAIL_ADDRESS,
-    password: process.env.MAIL_PASSWORD
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    user: process.env.SMTP_USER,
+    password: process.env.SMTP_PASSWORD
   }
 };
 
